0722800c9184cc728f1bce2b47b23769b7ce411d
hiroto.ohira <hiroto100114@gmail.com>
2022-12-20 22:49:20 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by hiroto.ohira on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
